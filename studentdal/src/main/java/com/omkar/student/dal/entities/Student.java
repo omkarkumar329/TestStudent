4c1158f847ef454726ab02647f9d00d79ffe3c38
Omkar Kumar <Omkarkumar329@gmail.com>
2022-06-10 10:08:48 +0530

    Updated student entity

--- studentdal/src/main/java/com/omkar/student/dal/entities/Student.java
+++ studentdal/src/main/java/com/omkar/student/dal/entities/Student.java
@@ -7,48 +7,56 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+//student class entity
 @Entity
-@Table(name="studenttab")
+@Table(name = "studenttab")
 public class Student {
-	
+
 	@Id
-	@GeneratedValue(strategy =GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	@Column(name="sname")
+	@Column(name = "sname")
 	private String name;
-	@Column(name="scourse")
+	@Column(name = "scourse")
 	private String Course;
-	@Column(name="sfee")
+	@Column(name = "sfee")
 	private Double fee;
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getCourse() {
 		return Course;
 	}
+
 	public void setCourse(String course) {
 		Course = course;
 	}
+
 	public Double getFee() {
 		return fee;
 	}
+
 	public void setFee(Double fee) {
 		this.fee = fee;
 	}
+
 	@Override
 	public String toString() {
 		return "Student [id=" + id + ", name=" + name + ", Course=" + Course + ", fee=" + fee + "]";
 	}
-	
-	
 
 }
