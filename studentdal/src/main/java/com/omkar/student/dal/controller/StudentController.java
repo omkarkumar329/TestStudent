f2b9116e4f0ce43a0fa561b05dd70777094f32a6
Omkar Kumar <Omkarkumar329@gmail.com>
2022-06-10 10:39:28 +0530

    added controller class

---
+++ studentdal/src/main/java/com/omkar/student/dal/controller/StudentController.java
@@ -0,0 +1,15 @@
+package com.omkar.student.dal.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+//student class entity
+@Controller
+public class StudentController {
+
+	
+}
